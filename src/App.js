806034e2310a1ad79663b7970ff9594735e0d616
aniruddha <ani@dctacademy.com>
2023-11-03 10:42:46 +0530

    charts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Container from "./components/Container"
+import Stats from "./components/Stats"
 export default function App() {
   return (
     <div>
       <h2>Redux Exercise</h2>
       <Container />
+      <Stats />
     </div>
   )
 }
---
+++ src/components/Stats.js
@@ -0,0 +1,25 @@
+// npm install react-chartkick chart.js
+import { PieChart } from 'react-chartkick'
+import 'chartkick/chart.js'
+import { useSelector } from 'react-redux'
+export default function Stats() {
+    const users = useSelector((state) => {
+        return state.users.data 
+    })
+
+    const frequency = users.reduce(function(acc, cv){
+        if(cv.gender === 'male') {
+            acc.male += 1
+        } else {
+            acc.female += 1
+        }
+        return acc 
+    }, { male: 0, female: 0})
+
+    return (
+        <div>
+            <h2>Stats</h2>
+            <PieChart data={Object.entries(frequency)}/>
+        </div>
+    )
+}
